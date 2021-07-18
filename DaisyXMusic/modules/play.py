86b03c044f08ce99aa68366da0d760783ac71ce0
crekha1 <80155306+crekha1@users.noreply.github.com>
2021-07-18 12:14:37 +0530

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -1278,7 +1278,7 @@ async def lol_cb(b, cb):
         await b.send_photo(chat_id,
             photo="final.png",
             reply_markup=keyboard,
-            caption=f"🎵**{atitle}** \n🕐 Duration : `{duration}` \n__Requested by__ : 👤 {r_by.mention}\n",
+            caption=f"🎵**{}**\n".format(queue[0][0]) \n🕐 Duration : `{duration}` \n__Requested by__ : 👤 {r_by.mention}\n",
         )
         
         os.remove("final.png")
